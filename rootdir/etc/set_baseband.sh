8bcd0f3cfeacee521bc472f46932fce3bcdb67cf
feroxx <frhatfrat@gmail.com>
2016-09-10 17:51:28 +0300

    Update set_baseband.sh

--- rootdir/etc/set_baseband.sh
+++ rootdir/etc/set_baseband.sh
@@ -3,7 +3,7 @@
 product=`getprop ro.build.product`
 # grep the modem partition for baseband version and set it
 case "$product" in
-        "u2")
+        "iuni" | "IUNI" | "u810" | "U810" | "U2" | "u2")
         setprop gsm.version.baseband `strings /firmware/image/modem.b21 | grep "^NBW" | head -1`
         ;;
 esac
