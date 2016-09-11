abd675978e4c79751b5c63f3343f5df780047695
feroxx <frhatfrat@gmail.com>
2016-09-11 19:40:05 +0300

    Update Android.mk

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -41,7 +41,7 @@ ifneq ($(call is-platform-sdk-version-at-least,17),true)
   LOCAL_CFLAGS += -include bionic/libc/kernel/common/linux/socket.h
   LOCAL_CFLAGS += -include bionic/libc/kernel/common/linux/un.h
 endif
-LOCAL_CFLAGS += -Wall -Werror
+LOCAL_CFLAGS += -Wall
 
 LOCAL_SRC_FILES := $(MM_CAM_FILES)
 
