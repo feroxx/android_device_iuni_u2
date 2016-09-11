cdbc3ae876d252f59009e81721db1ae2a972a9fb
feroxx <frhatfrat@gmail.com>
2016-09-11 10:35:04 +0300

    Update Android.mk

--- camerawrapper/Android.mk
+++ camerawrapper/Android.mk
@@ -1,22 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_C_INCLUDES := \
-    system/media/camera/include
-
-LOCAL_SRC_FILES := \
-    CameraWrapper.cpp
+LOCAL_MODULE := camera.msm8974
+LOCAL_SRC_FILES := CameraWrapper.cpp
 
 LOCAL_C_INCLUDES := \
     system/media/camera/include
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils
+    libhardware liblog libcamera_client libutils libcutils libdl
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera.msm8974
-
-LOCAL_MODULE_TAGS := optional
-
+LOCAL_CFLAGS := -Werror
 include $(BUILD_SHARED_LIBRARY)
