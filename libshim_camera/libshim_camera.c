151d16166907815fb68aece8e3139d6570762249
feroxx <frhatfrat@gmail.com>
2016-09-13 01:45:25 +0300

    Create libshim_camera.c

---
+++ libshim_camera/libshim_camera.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* TODO: Actually provide implementations for these function! */
+
+void _ZN7qcamera25QCamera2HardwareInterface20gnUpdatePictureFrameEv() {}
+void _ZN7qcamera25QCamera2HardwareInterface25gnUpdateFeatureParametersEv() {}
+void _ZN7qcamera25QCamera2HardwareInterface16updateParametersEPKcRb() {}
+void _ZN7qcamera19QCameraStateMachine26procEvtPreviewStoppedStateENS_21qcamera_sm_evt_enum_tEPv() {}
+void _ZN7qcamera19QCameraStateMachine16smEvtProcRoutineEPv() {}
+void _ZNK7android16CameraParameters6getStrEPKc() {}
