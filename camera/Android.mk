943af83f8d1adf76353f7629e68eaf0f5f1d9450
feroxx <frhatfrat@gmail.com>
2016-09-13 01:45:07 +0300

    Update Android.mk

--- camera/Android.mk
+++ camera/Android.mk
@@ -30,15 +30,3 @@ LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SHARED_LIBRARIES := liblog libcutils libgui libbinder libutils
-
-LOCAL_SRC_FILES := \
-    libshim_camera.c
-
-LOCAL_MODULE := libshim_camera
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-
-include $(BUILD_SHARED_LIBRARY)
