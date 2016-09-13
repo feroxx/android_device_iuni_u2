05cff57e94b5444dd492768023e1d9fcbdb4e3ac
feroxx <frhatfrat@gmail.com>
2016-09-14 01:50:25 +0300

    Update Android.mk

--- libshim_camera/Android.mk
+++ libshim_camera/Android.mk
@@ -17,12 +17,11 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SHARED_LIBRARIES := liblog libcutils libgui libbinder libutils
-
 LOCAL_SRC_FILES := \
     libshim_camera.c
 
+LOCAL_SHARED_LIBRARIES := libutils libgui liblog libbinder
 LOCAL_MODULE := libshim_camera
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
