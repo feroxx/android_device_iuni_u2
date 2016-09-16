211f181b1fea98c20878fdf1f23b70cc46a2442b
feroxx <frhatfrat@gmail.com>
2016-09-16 13:44:31 +0300

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -21,6 +21,7 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 TARGET_CONTINUOUS_SPLASH_ENABLED := true
+TARGET_BOOTANIMATION_HALF_RES := true
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
