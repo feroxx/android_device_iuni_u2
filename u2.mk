e2fb5dd48d65ce686ba030b4cb12f44994ef2952
feroxx <frhatfrat@gmail.com>
2016-10-13 14:00:10 +0300

    AUDIO AND NFC UPDATES

--- u2.mk
+++ u2.mk
@@ -137,8 +137,7 @@ PRODUCT_PACKAGES += \
  PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    device/iuni/u2/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    device/iuni/u2/nfc/libnfc-brcm-20791b05.conf:system/etc/libnfc-brcm-20791b05.conf \
+    device/iuni/u2/nfc/nfc-nci.conf:system/etc/nfc-nci.conf \
     device/iuni/u2/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
     
 # Graphics
