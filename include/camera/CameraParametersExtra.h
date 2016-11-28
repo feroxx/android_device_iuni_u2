94e8f9af75506c9b961a0f326bea39712fd5a4e5
feroxx <frhatfrat@gmail.com>
2016-11-28 14:50:10 +0200

    Update CameraParametersExtra.h

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -16,8 +16,10 @@
  
 #define CAMERA_PARAMETERS_EXTRA_C \
 const char CameraParameters::SCENE_MODE_GESTURE[] = "gesture"; \
-const char CameraParameters::SCENE_MODE_FOOD[] = "food";
+const char CameraParameters::SCENE_MODE_FOOD[] = "food"; \
+int CameraParameters::getStr(const char *key) const { return -1; };
  
 #define CAMERA_PARAMETERS_EXTRA_H \
     static const char SCENE_MODE_GESTURE[]; \
-    static const char SCENE_MODE_FOOD[];
+    static const char SCENE_MODE_FOOD[]; \
+    int getStr(const char *key) const;
