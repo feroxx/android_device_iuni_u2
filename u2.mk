046d0453454de1e87d28f27eb6610d33f3efaac1
feroxx <frhatfrat@gmail.com>
2017-02-23 00:35:00 +0300

    revert dt2w

--- u2.mk
+++ u2.mk
@@ -1,4 +1,4 @@
-#
+﻿#
 # Copyright (C) 2013 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -190,15 +190,6 @@ PRODUCT_PACKAGES += \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw 
-
-# Keyhandler
-PRODUCT_PACKAGES += \
-    com.cyanogenmod.keyhandler
-
-PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
-
-# never dexopt the keyhandler
-$(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
 
 # QCOM HAL
 $(call project-set-path,qcom-display,device/iuni/u2/display)
