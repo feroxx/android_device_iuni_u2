36376c3e72b41d39bb2d67f5208152ad3f050cf2
feroxx <frhatfrat@gmail.com>
2017-05-25 01:26:06 +0300

    Update Android.mk

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := libshim_camera.c
+LOCAL_SRC_FILES := libshim_camera.cpp
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
