8b656a7aa3d8aaff5e8fc0f2e968fe1961e8f363
feroxx <frhatfrat@gmail.com>
2017-06-20 17:13:51 +0300

    Update mm_camera_interface.c

--- camera/QCamera2/stack/mm-camera-interface/src/mm_camera_interface.c
+++ camera/QCamera2/stack/mm-camera-interface/src/mm_camera_interface.c
@@ -1303,7 +1303,7 @@ uint8_t get_num_of_cameras()
             break;
         }
 
-        if (strncmp(mdev_info.model, "msm_config", sizeof(mdev_info.model) != 0)) {
+        if (strncmp(mdev_info.model, "msm_config", size_t(mdev_info.model) != 0)) {
             close(dev_fd);
             dev_fd = 0;
             continue;
