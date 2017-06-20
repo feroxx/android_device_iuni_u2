67ed3d692e5cdc06c3b5442ca15748a2c0ceba5b
feroxx <frhatfrat@gmail.com>
2017-06-20 23:23:31 +0300

    fixes

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -61,10 +61,10 @@ LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface
 
 #Gionee <zhuangxiaojian> <2014-05-20> modify for CR01261494 begin
-ifeq "$(strip $(GN_CAMERA_FEATURE_SUPPORT))" "yes"
+
 LOCAL_SHARED_LIBRARIES += libgn_camera_feature
 LOCAL_C_INCLUDES += external/libgn_camera_feature/include
-endif
+
 #Gionee <zhuangxiaojian> <2014-05-20> modify for CR01261494 end
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
