31b49994e3c8d3df62e880db28f7a3c8604c4ced
feroxx <frhatfrat@gmail.com>
2017-06-23 13:13:56 +0300

    Update Android.mk

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
+
 MM_V4L2_DRIVER_LIST += msm8960
 MM_V4L2_DRIVER_LIST += msm8974
 MM_V4L2_DRIVER_LIST += msm8226
@@ -5,10 +7,12 @@ MM_V4L2_DRIVER_LIST += msm8610
 MM_V4L2_DRIVER_LIST += msm_bronze
 MM_V4L2_DRIVER_LIST += msm8916
 
-ifeq ($(call is-board-platform-in-list,$(MM_V4L2_DRIVER_LIST)),true)
+ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
   ifneq ($(USE_CAMERA_STUB),true)
     ifneq ($(BUILD_TINY_ANDROID),true)
       include $(call all-subdir-makefiles)
     endif
   endif
+endif
+
 endif
