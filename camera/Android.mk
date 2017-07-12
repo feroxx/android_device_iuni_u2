830d29d924d10107febb06006f31227a014e44c9
feroxx <frhatfrat@gmail.com>
2017-07-12 16:14:13 +0300

    ADD nuke camera

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,5 +1,3 @@
-ifeq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
-
 MM_V4L2_DRIVER_LIST += msm8960
 MM_V4L2_DRIVER_LIST += msm8974
 MM_V4L2_DRIVER_LIST += msm8226
@@ -7,12 +5,10 @@ MM_V4L2_DRIVER_LIST += msm8610
 MM_V4L2_DRIVER_LIST += msm_bronze
 MM_V4L2_DRIVER_LIST += msm8916
 
-ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
+ifeq ($(call is-board-platform-in-list,$(MM_V4L2_DRIVER_LIST)),true)
   ifneq ($(USE_CAMERA_STUB),true)
     ifneq ($(BUILD_TINY_ANDROID),true)
       include $(call all-subdir-makefiles)
     endif
   endif
-endif
-
 endif
