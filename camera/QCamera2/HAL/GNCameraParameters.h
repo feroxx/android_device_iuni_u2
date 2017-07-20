ec13e55d83539087b8084cad8a29c63a18ea09d1
feroxx <frhatfrat@gmail.com>
2017-07-21 00:31:22 +0300

    fix build

--- camera/QCamera2/HAL/GNCameraParameters.h
+++ camera/QCamera2/HAL/GNCameraParameters.h
@@ -183,7 +183,7 @@ private:
 
     int 	lookupAttr(const QCameraMap arr[], int len, const char *name);
 	String8 createValuesStringFromMap(const QCameraMap *map, int map_len);
-	String8 getStr(const values *values, int map_len);
+	String8 getStr(const values *values);
 
     // Map from strings to values
 	static const QCameraMap CAPTURE_MODES_MAP[];
