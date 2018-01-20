7a2f7f0e76db8406b2f7f511c9a920d7b8759d2d
feroxx <frhatfrat@gmail.com>
2018-01-20 22:39:33 +0300

    Revert "fix selinux"
    This reverts commit 5d8dacd2ecbd980802ad63d8ed4ae0a9757dc414.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,9 +138,9 @@ TARGET_PROVIDES_LIBLIGHT := true
 # keymaster
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
-# SELinux
-include device/qcom/sepolicy/sepolicy.mk
-include device/qcom/sepolicy/legacy-sepolicy.mk
+# selinux
+#include device/qcom/sepolicy/legacy-sepolicy.mk
+#include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/iuni/u2/sepolicy
 
 #TWRP
