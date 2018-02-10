f87e1f5466181bb88dfc187f11b6bd2805321156
feroxx <frhatfrat@gmail.com>
2018-02-11 01:11:02 +0300

    off mode again

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,6 +120,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/iuni/u2
 -include vendor/iuni/u2/BoardConfigVendor.mk
 
 # Offmode Charging
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
 BOARD_HEALTHD_CUSTOM_CHARGER_RES := device/iuni/u2/charger/images
 
 # Dexpreopt
