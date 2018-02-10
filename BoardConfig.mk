d6eae7867aa971996da078b5406b1c9c5b123707
feroxx <frhatfrat@gmail.com>
2018-02-10 21:07:43 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,8 +121,6 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/iuni/u2
 
 # Charger
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BOARD_CHARGER_ENABLE_SUSPEND := true
-BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
