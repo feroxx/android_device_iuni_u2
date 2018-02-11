0a2f84db6b5c3639ab6d442b7002a74792c36351
feroxx <frhatfrat@gmail.com>
2018-02-11 10:37:09 +0300

    Revert "Update BoardConfig.mk"
    This reverts commit 869f16a04943c4b01c0a30ba416a3b4c5c805b3f.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,6 +146,8 @@ TARGET_PROVIDES_LIBLIGHT := true
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
 # selinux
+include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/iuni/u2/sepolicy
 
 #TWRP
