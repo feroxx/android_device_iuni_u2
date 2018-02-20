66d672093473c49357df84c368d4e98f8987a131
feroxx <frhatfrat@gmail.com>
2018-02-21 01:58:59 +0300

    Update u2.mk

--- u2.mk
+++ u2.mk
@@ -44,7 +44,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/vendor/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/vendor/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/vendor/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:system/vendor/etc/media_profiles_V1_0.xml
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/vendor/etc/media_profiles.xml
 
 # Config files for touch and input
 PRODUCT_COPY_FILES += \
