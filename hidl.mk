2a9f8a245d5eb6576ffbe1a7e9d50a22220dc018
feroxx <frhatfrat@gmail.com>
2018-02-22 17:17:23 +0300

    Update hidl.mk

--- hidl.mk
+++ hidl.mk
@@ -12,8 +12,8 @@ PRODUCT_PACKAGES += \
 
 # Legacy Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
-    camera.device@1.0-impl-legacy
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl
 
 # Bluetooth
 PRODUCT_PACKAGES += \
