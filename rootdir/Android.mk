442429910e9460b48fbeda765483233a96f2b201
feroxx <frhatfrat@gmail.com>
2018-09-14 11:26:50 +0300

    fix bt and playback

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,6 +17,14 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.bt.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
 # Device init files
 
 include $(CLEAR_VARS)
