7c5597f03818ce8d35936ef6a840abfca017c69c
feroxx <frhatfrat@gmail.com>
2018-09-30 14:47:12 +0300

    Update hidl.mk

--- hidl.mk
+++ hidl.mk
@@ -17,7 +17,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.media.omx@1.0-impl
 
 # Graphics
 PRODUCT_PACKAGES += \
