21b7d1d5636223521d2278abe3b4f2d702b80ff3
feroxx <frhatfrat@gmail.com>
2018-10-11 18:42:03 +0300

    Update textclassifier package name for P

--- u2.mk
+++ u2.mk
@@ -100,7 +100,7 @@ PRODUCT_PACKAGES += \
 
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
-    textclassifier.smartselection.bundle1
+    textclassifier.bundle1
 
 PRODUCT_PACKAGES += \
     hostapd.accept \
